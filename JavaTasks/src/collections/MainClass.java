180f7d60cba9a3aa61509895c332dd92c44b60cf
SelivanovAlexey <fateisdecease@gmail.com>
2018-11-18 08:45:10 +0300

    Refactored main method

--- JavaTasks/src/collections/MainClass.java
+++ JavaTasks/src/collections/MainClass.java
@@ -1,9 +1,13 @@
 package collections;
 
-import java.util.Iterator;
 
 public class MainClass {
     public static void main(String[] args) {
+
+        test();
+    }
+
+    private static void test (){
 
         ILinkedList<String> list = new MyLinkedList();
 
@@ -13,76 +17,26 @@ public class MainClass {
         list.add("154");
         list.add("934");
 
-
         System.out.println("Source list");
         System.out.println(list);
         System.out.println();
 
-
         System.out.println("Inserting");
-        System.out.println(list);
-
         list.add(3, "22");
-
-        System.out.println(list);
-        System.out.println();
-
-
-
         System.out.println("Getting element");
-        System.out.println(list);
-
         System.out.println(list.get(4));
-
-        System.out.println();
-
-
-
         System.out.println("Getting index of element");
-        System.out.println(list);
-
         System.out.println(list.indexOf("32"));
-
-        System.out.println();
-
-
-
-        System.out.println("Setting new value");
-        System.out.println(list);
-
-        System.out.println("Old value = " + list.set(1, "10"));
-
-        System.out.println(list);
-        System.out.println();
-
-
-
         System.out.println("Removing element");
-        System.out.println(list);
-
         System.out.println("Old value = " + list.remove(4));
-
-        System.out.println(list);
-        System.out.println();
-
-
         System.out.println("Removing element from end");
-        System.out.println(list);
-
         System.out.println("Old value = " + list.remove());
-
-        System.out.println(list);
-        System.out.println();
-
-
         System.out.println("Clearing the list");
-
         list.clear();
 
+
         System.out.println(list);
 
-
     }
-
 
 }
